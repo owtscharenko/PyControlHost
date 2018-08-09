83c60fe14fa41541f0dc1dd47eaa65ba7a0fb311
owtscharenko <owtscharenko@physik.uni-bonn.de>
2018-08-09 15:20:42 +0200

    ENH: unravel ship data in .h5 file to folder per run with one bytefile per spill

---
+++ PyControlHost/unpack_runfiles.py
@@ -0,0 +1,100 @@
+from __future__ import division
+
+import os
+import numpy as np
+import tables as tb
+import datetime
+from numba import njit, jit
+from ship_data_converter import FrHeader, Hit
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - [%(levelname)-8s] (%(threadName)-10s) %(message)s')
+logger = logging.getLogger('Converter')
+
+
+def get_file_date(cycleID):
+    ''' convert spillID / cycleID to human readable date'''
+    
+    start_date = datetime.datetime(2015, 04, 8, 13, 00) # careful: python datetime is 1 hour behind c time.h module. TODO: check why
+    return (start_date + datetime.timedelta(seconds = cycleID / 5.)).strftime("%Y_%m_%d_%H_%M_%S")
+
+
+def unpack_run_file(input_file):
+    partID = input_file.split('_')[-3]
+    run_number = input_file.split('_')[-1].split('.')[0]
+    directory = os.path.dirname(input_file)
+    spillpath =  "%s/part_%s/RUN_%s/" % (directory, partID, run_number)
+    logger.info('Unpacking partition %s run %s' % (partID, run_number))
+    if not os.path.exists(spillpath):
+                os.makedirs(spillpath)
+
+    with tb.open_file(input_file, mode='r') as in_file:
+        for group in in_file.walk_groups():
+            spill_number = group._v_name
+
+            if spill_number == '/':
+                continue
+            
+            else:
+                spill_number = spill_number.split('_')[-1]
+                file_name =  get_file_date(np.uint32(spill_number))
+#                 logger.info('spill %s corresponds to date %s' %(spill_number,file_name))
+                headers_in = group.Headers[:]
+                hits_in = group.Hits[:]
+                logger.info('found %s hits in %s frames for spill %s / %s' %(hits_in.shape[0], headers_in.shape[0], spill_number, file_name))
+                i = 0
+                with open(spillpath + file_name + '.txt', 'w') as spillfile:
+                    for row in range(0,headers_in.shape[0]):
+                        try:
+                            nhits = int((headers_in[row]['size']-16)/4)
+                         
+                            print nhits
+                            if nhits <= 0 :
+                                logger.warning('header %s has size %s'% (row,nhits))
+                                if row > 0:
+                                    logger.error('empty header in between frames')
+                                    raise RuntimeWarning
+                                continue
+                            else:
+                                header_out = np.zeros(shape=(1,), dtype = FrHeader)
+                                hits_out = np.zeros(shape = nhits, dtype = Hit)
+                                
+                                header_out['size'] = headers_in[row]['size']
+                                header_out['partID'] = headers_in[row]['partID']
+                                header_out['cycleID'] = headers_in[row]['cycleID']
+                                header_out['frameTime'] = headers_in[row]['frameTime']
+                                header_out['timeExtent'] = headers_in[row]['timeExtent']
+                                header_out['flags'] = headers_in[row]['flags']
+                                
+                                hits_out['channelID'] = hits_in[i:(i+nhits)]['channelID']
+                                hits_out['hit_data'] = hits_in[i:(i+nhits)]['hit_data']
+    #                             np.savetxt(spillfile, headers[row].reshape((1,)))
+    #                             np.savetxt(spillfile, hits[i:(i+nhits)])
+#                                 headers[row].tofile(spillfile)
+#                                 hits[i:(i+nhits)].tofile(spillfile)
+                                header_out.tofile(spillfile)
+                                print header_out
+                                hits_out.tofile(spillfile)
+                                print hits_out
+                                i += nhits
+                        except RuntimeWarning:
+                            logger.info('ended loop')
+                        
+                
+def check_bytefiles(input_bytefile):
+    with open(input_bytefile, 'rb') as in_file:
+        data  = list(in_file)
+        print data[1]
+        
+if __name__ == '__main__':
+    files = ['/media/data/SHiP/charm_exp_2018/test_data_converter/elsa_testbeam_data/take_data/module_0/36_module_0_ext_trigger_scan.h5',
+             '/media/data/SHiP/charm_exp_2018/test_data_converter/elsa_testbeam_data/take_data/module_1/36_module_1_ext_trigger_scan.h5',
+             '/media/data/SHiP/charm_exp_2018/test_data_converter/elsa_testbeam_data/take_data/module_2/36_module_2_ext_trigger_scan.h5',
+             '/media/data/SHiP/charm_exp_2018/test_data_converter/elsa_testbeam_data/take_data/module_3/36_module_3_ext_trigger_scan.h5',
+             '/media/data/SHiP/charm_exp_2018/test_data_converter/elsa_testbeam_data/take_data/module_0/37_module_0_ext_trigger_scan.h5',
+             '/media/data/SHiP/charm_exp_2018/test_data_converter/elsa_testbeam_data/take_data/module_1/37_module_1_ext_trigger_scan.h5',
+             '/media/data/SHiP/charm_exp_2018/test_data_converter/elsa_testbeam_data/take_data/module_2/37_module_2_ext_trigger_scan.h5',
+             '/media/data/SHiP/charm_exp_2018/test_data_converter/elsa_testbeam_data/take_data/module_3/37_module_3_ext_trigger_scan.h5',]
+    unpack_run_file('/media/data/ship_charm_xsec_Jul18/run_data/partition_0x800_run_2467.h5')
+    check_bytefiles('/media/data/ship_charm_xsec_Jul18/run_data/part_0x800/RUN_2467/2018_07_26_16_44_10.txt')
